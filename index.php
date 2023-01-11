2601861267d2eeb585584367af0de1db33ce0539
HeyArHei <61173315+heihei0314@users.noreply.github.com>
2023-01-11 14:45:53 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<html>
+  <body>
+    <div name="input">
+      Seacrh an Athlete<br>
+      <form>
+        <input id='athleteName'> </input>
+      </form>
+      
+     </div>
+  </body>
+</html>
