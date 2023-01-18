6c597c5a47bff329bf3fc7f47ee3755ae84ee9ed
Tsz Hei YEUNG <heihei0314@gmail.com>
2023-01-18 15:07:37 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+A simple calculator class in Python
+'''
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+    
+    def subtract(self, a, b):
+        return a - b
