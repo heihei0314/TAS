be4fa3f17dda1fd7c7f684f0e90b67fadc8685b8
Tsz Hei YEUNG <heihei0314@gmail.com>
2023-02-05 19:52:42 +0800

    Update putMean.php

--- resource/putMean.php
+++ resource/putMean.php
@@ -1,8 +1,29 @@
 	<?php 
-		$court = $_GET['court'];
-		$referee = $_GET['referee'];
-		$RB = $_GET['RB'];
-		$scoreType = $_GET['scoreType'];
+		$name = "";
+		$win = "";
+		$lose = "";
+		$WinningRound = "";
+		$Score = "";
+		$Punch = "";
+		$Body = "";
+		$SpinBody = "";
+		$SpinHead = "";
+		$Head = "";
+		$Warning = "";
+		
+		if(isset($_POST["name"])){
+        		$name = $_POST["name"];
+			$win = $_POST["win"];
+			$lose = $_POST["lose"];
+			$WinningRound = $_POST["WinningRound"];
+			$Score = $_POST["Score"];
+			$Punch = $_POST["Punch"];
+			$Body = $_POST["Body"];
+			$SpinBody = $_POST["SpinBody"];
+			$SpinHead = $_POST["SpinHead"];
+			$Head = $_POST["Head"];
+			$Warning = $_POST["Warning"];
+    		}
 		// Establishing Connection with Database
 		require_once '../conf/db_configs.php';
 		$conn = new mysqli(host, username, password, dbname);
@@ -10,11 +31,14 @@
 			die("Connection failed: " . $conn->connect_error);
 		} 
 		// Establishing Connection with Database
-		$sql = "INSERT INTO scoreevent (court, referee, RB, scoreType) VALUES ('$court', '$referee', '$RB', '$scoreType')";
+		$sql = "INSERT INTO profile (name, win, lose, WinningRound, Score, Punch, Body, SpinBody, SpinHead, Head, Warning) VALUES ('$name', '$win', '$lose', '$WinningRound', '$Score', '$Punch', '$Body', '$SpinBody', '$SpinHead', '$Head', '$Warning')";
 		if ($conn->query($sql) === TRUE) {
 			echo $sql;
 		} else {
-			echo "Error: " . $sql . "<br>" . $conn->error;//*/
+			$sql = "UPDATE profile (name, win, lose, WinningRound, Score, Punch, Body, SpinBody, SpinHead, Head, Warning) VALUES ('$name', '$win', '$lose', '$WinningRound', '$Score', '$Punch', '$Body', '$SpinBody', '$SpinHead', '$Head', '$Warning')";
+			if ($conn->query($sql) === TRUE) {
+				echo $sql;
+			} else {
 		}
 		
 		$conn->close();
