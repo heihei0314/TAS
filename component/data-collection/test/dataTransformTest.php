001551fe463179955b9eccd89559b56fd938beef
Tsz Hei YEUNG <heihei0314@gmail.com>
2023-02-06 21:13:07 +0800

    Update dataTransformTest.php

--- component/data-collection/test/dataTransformTest.php
+++ component/data-collection/test/dataTransformTest.php
@@ -5,56 +5,46 @@
     $s=0;
     $f=0;
     
-    //activate test     
-    $expectedWinlose = Array("","Win","Lose","Lose","Win");
-
     //stub athletes name for test (null, one entry data, three entries data)
     $athletesTest = Array("","Lam Ching Ho","Kong Hin Sing");
-    print_r($athletesTest);
-
-    $msg = transformDataTest($athletesTest,$expectedWinlose);
-    consoleLog($msg);
+    $testData = array();
+    $testWinlose = array();
+    //activate test   
+    foreach ($athletesTest as $athlete){
+        $testData = transformDataTest($athlete);
+        array_push($testWinlose,$testData);
+        $t++;
+    }   
+    
     
     //pring test report
     summary();
     
     // test data transformation
-    function transformDataTest($athletesTest,$expectedWinlose){
-        global $t;
-        global $s;
-        global $f;
-        
+    function transformDataTest($athlete){        
         include '../dataTransform.php';
         $dataTransform = new dataTransform();
         foreach ($athletesTest as $athlete){
             //transform and insert data
             $dataTransform->transformData($athlete);
-            $t++;
         }        
         
         //get data from db for validate
         require_once __DIR__.'/../../../resource/controller.php';
         $controller = new Controller();
-        $testWinlose=array("");
-        $testData = array();
+        $gameData = array();
+        $test = array();
         foreach ($athletesTest as $athlete){
-            $testData = $controller->getGameData($athlete);
-            foreach ($testData as $data){
-                array_push($testWinlose,$data['winlose']);
+            $gameData = $controller->getGameData($athlete);
+            foreach ($gameData as $data){
+                array_push($test,$data['winlose']);
             }
         }  
-        print_r($testWinlose);
-        
-        if($testWinlose==$expectedWinlose){
-            $s++;
-            $msg = 'Transform data succeed';
-        }
-        else{
-            $f++;
-            $msg = 'Wrong Win Lose data';
-        }
-        return $msg;
+        print_r($test);
+        return $test;
     }
+
+       
     
     function consoleLog($text){
         echo "<script>console.log('".$text."');</script>";
@@ -65,7 +55,18 @@
         global $t;
         global $s;
         global $f;
-        
+        global $testWinlose;
+        $expectedWinlose = Array(array(""),array("Win"),array("Lose","Lose","Win"));  
+        foreach ($testWinlose as $result){
+            if($result==$expectedWinlose){
+                $s++;
+                consoleLog('Transform data succeed');
+            }
+            else{
+                $f++;
+                consoleLog('Wrong Win Lose data');
+            }
+        }
         $summary = "test cases: ".$t.", success: ".$s.", fail: ".$f;
         consoleLog($summary);
     }
