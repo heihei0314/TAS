fc10b07fb149d81b7aa8021b058847605f0a57c5
Eric Yeung <ericyeung.ust.hk>
2023-02-09 15:19:18 +0800

    modified:   test/integrationTest.php

--- test/integrationTest.php
+++ test/integrationTest.php
@@ -7,6 +7,19 @@
             $controller = new Controller();
             $allAthletes = $controller->getAthletes();
             $this->assertGreaterThan(0,count($response));
-            $this->assertEquals("Lam Ching Ho",$response[0]['name']);
+            $this->assertEquals("Lam Ching Ho",$testCase1);
+            $testCase1 = $response[0]['name'];
+            $mean1 = getMean($testCase1);
+            $this->assertEquals(3, $mean1['Score']);
+            
+            $testCase2 = $response[0]['name'];
+            $mean2 = getMean($testCase2);
+            $this->assertEquals(15, $mean2['Score']);
+        }
+        public function getMean($case){
+            require_once __DIR__.'/../component/data-analyser/analyser.php';
+            $analyser = new Profile();
+            $profile = $analyser->calculateMean($case);
+            return $profile;
         }
     }
