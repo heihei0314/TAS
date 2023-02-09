53a7472e0a7c7a2a00c967ecce28df6af9e63fb1
Eric Yeung <ericyeung.ust.hk>
2023-02-09 16:45:49 +0800

    modified:   test/integrationTest.php

--- test/integrationTest.php
+++ test/integrationTest.php
@@ -13,26 +13,29 @@
             //2. transform specific athlete game data
             require_once __DIR__.'/../component/data-collection/dataTransform.php';
             $dataTransform = new dataTransform();
-            $dataset = $dataTransform->transformData($allAthletes[0]);   
+            $gameData1 = $dataTransform->transformData($allAthletes[0]);   
             $this->assertEquals(1, count($gameData1));
             print_r(count($gameData1));echo "<br><br>";
 
-            //$gameData2 = $this->testDataTransform($allAthletes[1]);
-           // $this->assertEquals(3, count($gameData2));
-            //print_r(count($gameData2));echo "<br><br>";
+            $gameData2 = $dataTransform->transformData($allAthletes[1]);   
+            $this->assertEquals(3, count($gameData2));
+            print_r(count($gameData2));echo "<br><br>";
 
             //3. calculate mean for specific athlete with DB
-            $mean1 = $this->testGetMean($allAthletes[0]);
-            print_r( $mean1);echo "<br><br>";
+            require_once __DIR__.'/../component/data-analyser/analyser.php';
+            $analyser = new Profile();
+            $mean1 = $analyser->calculateMean($allAthletes[0]);
+            print_r($mean1);echo "<br><br>";
             $this->assertEquals(3, $mean1['Score']);
 
-            $testCase2 = $allAthletes[1];
-            $mean2 = $this->testGetMean($allAthletes[1]);
-            print_r( $mean2);echo "<br><br>";
+            $mean1 = $analyser->calculateMean($allAthletes[1]);
+            print_r($mean2);echo "<br><br>";
             $this->assertEquals(15, $mean2['Score']);
 
             //4. calculate win rate from grabed mean data
-            $winRateTestResult = $this->testGetWinRate($mean1,$mean2);
+            require_once __DIR__.'/../component/data-analyser/winRate.php';
+            $winRateAnalyser = new winRateCalculator();
+            $winRateTestResult = $winRateAnalyser->getWinRate($mean1,$mean2);
             $this->assertEquals(array('0%','100%'), $winRateTestResult);
             print_r($winRateTestResult);echo "<br><br>";
 
